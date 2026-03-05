c6f67751f85a661d09f3bf4c74bdf66e0e073de4
AhmedMagdy-GH <am4474646@gmail.com>
2026-03-05 19:56:56 +0200

    Add OOP coffee machine

---
+++ Oop_Coffee_Machine/main.py
@@ -0,0 +1,40 @@
+from menu import Menu
+from coffee_maker import CoffeeMaker
+from money_machine import MoneyMachine
+
+menu = Menu()
+money_machine = MoneyMachine()
+coffee_maker= CoffeeMaker()
+
+                  # MINE
+# while True:
+#     payment =0
+#     user_order = input(f"What would you like? ({menu.get_items()}): ")
+#     if user_order == "report":
+#         coffee_maker.report()
+#         money_machine.report()
+#         continue
+#     elif user_order == "off":
+#         break
+#
+#     order_info= menu.find_drink(user_order)
+#
+#     if coffee_maker.is_resource_sufficient(order_info):
+#         money_machine.make_payment(order_info.cost)
+#
+#     coffee_maker.make_coffee(order_info)
+
+
+is_on = True
+while is_on:
+    options = menu.get_items()
+    choice =input(f"What would you like? ({options}: ")
+    if choice == "off":
+        is_on = False
+    elif choice == "report":
+        coffee_maker.report()
+        money_machine.report()
+    else:
+        drink = menu.find_drink(choice)
+        if coffee_maker.is_resource_sufficient(drink) and money_machine.make_payment(drink.cost) :
+            coffee_maker.make_coffee(drink)
--- Test.py
+++ Test.py
@@ -1,14 +1,19 @@
-# Target is the number up to which we count
-def fizz_buzz(target):
-    for number in range(1, target + 1):
-        if number % 3 == 0 and number % 5 == 0:
-            print("FizzBuzz")
-        elif number % 3 == 0:
-            print("Fizz")
-        elif number % 5 == 0:
-            print("Buzz")
-        else:
-            print([number])
+class User():
+    def __init__(self,user_id,username):
+        self.id = user_id
+        self.username=username
+        self.followers=0
+        self.following=0
+    def follow(self,user):
+        user.followers+=1
+        self.following+=1
 
 
-fizz_buzz(15)
+user1= User("001","ahmed")
+user2= User("002","mazin")
+
+print(user1.follow(user2))
+print(user1.following)
+print(user1.followers)
+print(user2.following)
+print(user2.followers)
